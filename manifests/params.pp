29d07e40b7b937c078f1f17ea6dbcf7722be216a
root <root@lvicpuppetd02.ingramcontent.com>
2015-04-29 11:21:35 -0500

    Revert to 78d4cbea24c8df4e3c139eb2aea6a724c71ddb2d

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,39 +1,11 @@
-<<<<<<< HEAD
-# == Class: sssd::params
-# Set up parameters that vary based on platform or distribution.
-# 
-# UID_MIN is the lowest uid allowed for use by non-system accounts.
-# UID_MIN is often defined in /etc/login.defs
-# As of RedHat Enterprise Linux 6, UID_MIN is still 500.
-# But in Fedora >= 16, UID_MIN has been changed to 1000.
-# https://fedoraproject.org/wiki/Features/1000SystemAccounts
-# I'm choosing to set 1000 as the default here, to be safe and
-# forward-compatible.
-#
-# === Examples
-# class { 'sssd::params': }
-#
-# === Authors
-# Nicholas Waller <code@nicwaller.com>
-#
-# === Copyright
-# Copyright 2013 Nicholas Waller, unless otherwise noted.
-=======
 # == Class sssd::params
 #
 # This class is meant to be called from sssd
 # It sets variables according to platform
->>>>>>> sssdPAM
 #
 class sssd::params {
   case $::osfamily {
     'RedHat': {
-<<<<<<< HEAD
-      $dist_uid_min = 1000
-    }
-    default: {
-      fail('Unsupported distribution')
-=======
       $sssd_package_name       = 'sssd'
       $service_name            = 'sssd'
       $config_file             = '/etc/sssd/sssd.conf'
@@ -54,6 +26,8 @@ class sssd::params {
       $mkhomedir               = 'disabled'
       $enable_mkhomedir_cmd    = '/usr/sbin/authconfig --enablemkhomedir --update'
       $disable_mkhomedir_cmd   = '/usr/sbin/authconfig --disablemkhomedir --update'
+      $authconfig_pam_cmd      = '/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update'
+      $authconfig_enable       = true
       $pam_mkhomedir_check     = '/bin/grep -E \'^USEMKHOMEDIR=yes$\' /etc/sysconfig/authconfig'
       $manage_idmap            = true
       $idmap_package_name      = 'libsss_idmap'
@@ -67,7 +41,6 @@ class sssd::params {
     }
     default: {
       fail("${::operatingsystem} not supported")
->>>>>>> sssdPAM
     }
   }
 }
